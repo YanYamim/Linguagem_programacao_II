81bd1c222792cecf82a376d041bafee564928105
YanYamim <yanyamim@gmail.com>
2024-11-27 14:22:36 -0300

    Pequena correção

--- src/edu/fatec/lp2/Calculadora/Main.java
+++ src/edu/fatec/lp2/Calculadora/Main.java
@@ -2,10 +2,10 @@ package edu.fatec.lp2.Calculadora;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Calculadora.calculate("+", 10.0, 20.0, 30.0));
-        System.out.println(Calculadora.calculate("-", 50.0, 15.0, 5.0)); 
-        System.out.println(Calculadora.calculate("*", 2.0, 3.0, 4.0));   
-        System.out.println(Calculadora.calculate("/", 100.0, 5.0, 2.0)); 
+        System.out.println(Calculadora.calculate("+", 10.0, 20.0));
+        System.out.println(Calculadora.calculate("-", 50.0, 15.0)); 
+        System.out.println(Calculadora.calculate("*", 2.0, 3.0));
+        System.out.println(Calculadora.calculate("/", 100.0, 5.0)); 
     }
 }
     
